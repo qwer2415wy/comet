b1a990ac6c13fea9a157e3327bbbe31a9835edc4
太陽 <qwer2415wy@gmail.com>
2020-12-07 18:23:28 +0800

    Comet

--- dashboard/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-ob_start();
-setcookie('nm_username', '', time() - 10, '/');
-setcookie('nm_password', '', time() - 10, '/');
-session_destroy();
-header('Location: login.php');
